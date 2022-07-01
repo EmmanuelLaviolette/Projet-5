2186d928df9c9627b5f7bcdfcb6f34132b2f8299
Elaviolette <e.laviolette@live.fr>
2022-07-02 01:27:11 +0200

    Fin page produit

--- back/product.js
+++ back/product.js
@@ -3,8 +3,6 @@
 let params = (new URL(document.location)).searchParams;
 let $id = params.get('id');
 
-console.log($id);
-
 fetch("http://localhost:3000/api/products")
     .then(function(res) {
         if (res.ok) {
@@ -14,41 +12,50 @@ fetch("http://localhost:3000/api/products")
     .then(function(products) {
 
 
-
         for (let product of products) {
 
             if (product._id == $id) {
 
+                // let selectItemImg = document.getElementByClassName('item__img');
+                // console.log(selectItemImg)
+
                 let newImage = document.createElement('img');
                     newImage.setAttribute('src', product.imageUrl);
                     newImage.setAttribute('alt', product.altTxt);
+                    console.log(newImage);
+
+                // selectItemImg.append(newImage);
 
                 let newName = document.getElementById('title');
                     newName.innerText = product.name;
 
                 let newPrice = document.getElementById('price');
-                    newPrice.innerText = product.price
+                    newPrice.innerText = product.price;
 
                 let newDescription = document.getElementById('description');
                     newDescription.innerText = product.description;
 
 
-                for (let i=0; i<product.colors.length; i++) {
-                    console.log(product.colors);
+                for (let color of product.colors) {
+
+                    let selectOption = document.getElementById('colors');
 
                     let newColor = document.createElement('option');
-                    newColor.setAttribute('value', product.colors);
+                        newColor.setAttribute('value', color);
+                        newColor.innerText = color;
+
+                    selectOption.append(newColor);
 
                 }
 
-            } else {
-                console.log('Erreur');
-            }
 
-            // const itemImg = document.getElementByClassName('item__img');
-            // console.log(itemImg);
+            } //else {
+            //     console.log('Erreur');
+            // };
+
+
             
-            // itemImg.append(newImage);
+            // 
 
         }
 
